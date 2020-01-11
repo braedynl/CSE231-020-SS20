461ecfde21c3ba1f4b7b3f68a3d1aa96d813038e
Braedyn L <53410383+braedynl@users.noreply.github.com>
2020-01-10 21:02:29 -0500

    Add files via upload

---
+++ Lab 01/lab01.py
@@ -0,0 +1,20 @@
+# Lab01 - Example Answer (No Comments)
+# Braedyn Lettinga
+
+import math
+
+A = float( input( "\nEnter the coefficient A: " ) )
+B = float( input( "\nEnter the coefficient B: " ) )
+C = float( input( "\nEnter the coefficient C: " ) )
+
+print( "\nThe coefficients of the equation:\n" )
+print( "  Coefficient A = ", A )
+print( "  Coefficient B = ", B )
+print( "  Coefficient C = ", C )
+
+root1 = (-B + math.sqrt((B**2) - 4 * A * C))/(2 * A)
+root2 = (-B - math.sqrt((B**2) - 4 * A * C))/(2 * A)
+
+print( "\nThe roots of the equation:\n" )
+print( "  Root #1 = ", round(root1, 3) ) 
+print( "  Root #2 = ", round(root2, 3) )
