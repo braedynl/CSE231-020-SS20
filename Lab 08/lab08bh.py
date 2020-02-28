1c184f4b65326c23114db6746b720b3755e4bae5
Braedyn L <braedynlettinga@gmail.com>
2020-02-28 16:10:43 -0500

    Add files via upload

---
+++ Lab 08/lab08bh.py
@@ -0,0 +1,27 @@
+'''
+There are many, many ways to go about this. Dr. Enbody
+intentionally left this one open-ended to see where you guys
+go and how you think. 
+I'll be giving you guys a game-plan, but we want to see how
+ya'll organize.
+'''
+
+# open the two datasets, "data1.txt" and "data2.txt",
+# you don't make a prompt for these, hardcode it
+
+# Initialize a dictionary to hold the names and scores
+
+# Instead of copying and pasting code to read-in both
+# datasets into the dictionary, it might be a good idea
+# to make a function that can read-in either? 🤔
+
+# You can use these print statements for the output, make
+# sure you sort by name first!
+# Also make sure your totals are ints!
+
+# print("{:10s}{:<10s}".format("Name", "Total"))
+# for name, total in ___?: 
+    # print("{:10s}{:<10d}".format(name, total))
+
+# You can make a main() function if you want, it's
+# optional. Make sure to call it! 
