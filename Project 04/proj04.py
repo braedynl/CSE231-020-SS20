7587f628daf14703f38d5cdf56137c3a414a5f19
Braedyn Lettinga <braedynlettinga@gmail.com>
2020-09-16 16:30:02 -0400

    project 4 added

---
+++ Project 04/proj04.py
@@ -0,0 +1,95 @@
+''' Insert heading comments here.'''
+
+
+def display_options():
+    ''' This function displays the menu of options'''
+
+    MENU = '''\nPlease choose one of the options below:
+             A. Convert a decimal number to another base system         
+             B. Convert decimal number from another base.
+             C. Convert from one representation system to another.
+             D. Display the sum of two binary numbers.
+             E. Compress an image.
+             U. Uncompress an image.
+             M. Display the menu of options.
+             X. Exit from the program.'''
+       
+    print(MENU)
+    
+def numtobase( N, B ):
+    '''Insert docstring here.'''
+    pass  # insert your code here
+
+def basetonum( S, B ):
+    '''Insert docstring here.'''
+    pass  # insert your code here
+
+def basetobase(B1,B2,s_in_B1):
+    '''Insert docstring here.'''
+    pass  # insert your code here
+
+def addbinary(S, T):
+    '''Insert docstring here.'''
+    pass  # insert your code here
+    
+ 
+def compress(S):
+    '''Insert docstring here.'''
+    pass  # insert your code here
+
+
+def uncompress(S):
+    '''Insert docstring here.'''
+    pass  # insert your code here         
+
+def main():
+    BANNER = '''
+        .    .        .      .             . .     .        .          .          .
+         .                 .                    .                .
+  .               A long time ago in a galaxy far, far away...   .
+              .   A terrible civil war burns throughout the  .        .     .
+                 galaxy: a rag-tag group of freedom fighters   .  .
+     .       .  has risen from beneath the dark shadow of the            .
+.        .     evil monster the Galactic Empire has become.                  .  .
+    .      Outnumbered and outgunned,  the Rebellion burns across the   .    .
+.      vast reaches of space and a thousand-thousand worlds, with only     .
+    . their great courage - and the mystical power known as the Force -
+     flaming a fire of hope.                                    .
+
+              .------.
+            .'::::::' `.
+            |: __   __ |
+            | <__] [__>|
+            `-.  __  .-'
+              | |==| |
+              | |==| |
+           __.`-[..]-`\__
+    _.--:``      ||   _``:::--._
+   | |  |.      .:'  (o) ::|  | |
+   |_|  |::..  // _       :|  |_|
+    ===-|:``` // /.\       |-===
+   |_| `:___//_|[ ]|_____.' |_| )
+    l=l   |\V/_=======_==|   l=l/
+  .-l=l   |`'==/=="======|  /|.:
+  | l l   |=="======\=_==| `-T l
+  `.l_l   |==============|   l_l
+    [_]  [__][__]____[_]__]  [_]
+    \\\ .'.--.- --   --. .`. |||.
+    \\\\| |  |    |    |  || ||||
+     \\\\   .'    |    |  |`.||||   
+      \\\\  |  LS |    `.   |||||     
+
+
+    
+  ~~ Your mission: Tatooine planet is under attack from stormtroopers,
+                   and there is only one line of defense remaining        
+                   It is up to you to stop the invasion and save the planet~~    
+                
+    '''
+
+    print(BANNER)
+    
+    pass  # insert your code here
+
+if __name__ == "__main__": 
+    main()
--- README.md
+++ README.md
@@ -48,7 +48,7 @@ Your co-pilot on this expedition through CSE231 will be the course schedule, whi
 
 ## Course Schedule
 
-Last Refresh: 09/16/2020 12:55 PM EST
+Last Refresh: 09/16/2020 04:29 PM EST
 
 The assignments listed on the schedule have links to the corresponding website they are hosted on. Links to Mimir and D2L assignments are not direct because a login is required. You can hover over the assignment's text to see its precise due date (sorry mobile users).
 
@@ -304,12 +304,12 @@ Sometimes, you might be faced with a problem that you just have no idea how to i
 
 Stay safe! These trackers are updated on a daily basis, and are scraped directly from the State of Michigan's [website](https://www.michigan.gov/Coronavirus).
 
-- **Total Confirmed Cases:** 113,183
-- **Total COVID-19 Deaths:** 6,612
-- **Daily Confirmed Cases:** 571
+- **Total Confirmed Cases:** 113,863
+- **Total COVID-19 Deaths:** 6,623
+- **Daily Confirmed Cases:** 680
 - **Daily COVID-19 Deaths:** 11
 
-Last Refresh: 09/16/2020 12:55 PM EST
+Last Refresh: 09/16/2020 04:29 PM EST
 
 <div align="center"><img src="assets/images/covid_data.png"></img></div>
 
