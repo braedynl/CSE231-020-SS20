f4e1454fe14e7d2991b1af2b9970b1040d752ac5
Braedyn L <braedynlettinga@gmail.com>
2020-09-24 18:58:30 -0400

    refresh 9/24/20, proj05 added

---
+++ Project 05/proj05.py
@@ -0,0 +1,22 @@
+def open_file():
+	# insert doc string here
+	pass
+
+def str_plot(month,week,n,type):
+	# insert doc string here
+	pass
+        
+def convert(n):
+	# insert doc string here
+	pass
+
+def month_name(n):
+	# insert doc string here
+	pass
+   
+def main():     
+	# insert doc string here
+	pass
+        
+if __name__ == '__main__':
+    main()
