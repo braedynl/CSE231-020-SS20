51c7ffbee2297e8eb425b261a7561f88365b9a7a
Braedyn L <braedynlettinga@gmail.com>
2020-10-25 13:45:51 -0400

    refresh 10/25/20

---
+++ Solutions/lab05_ans.py
@@ -0,0 +1,78 @@
+'''
+CSE231 - Introduction to Programming I
+Lab 5
+Example Solution
+
+Author: Braedyn Lettinga
+'''
+
+fp = open("data.txt", "r")
+
+# .readline() returns the line that it "read", we
+# can simply take it and add the "BMI" title to the end
+print(fp.readline().strip() + "  BMI")
+
+# Averages
+avg_height = 0
+avg_weight = 0
+avg_bmi = 0
+
+# Maximums
+max_height = 0
+max_weight = 0
+max_bmi = 0
+
+# Minimums
+min_height = 10**6
+min_weight = 10**6
+min_bmi = 10**6
+
+# We're going to use a tracker variable, 'n',
+# to count the number of people in the file
+n = 0
+
+for line in fp:
+    line = line.strip()
+
+    # extract the necessary info, converting types appropriately
+    height = float(line[12:16])
+    weight = float(line[24:29])
+
+    bmi = weight / ((height)**2)
+
+    # For each person, we're going to add to our
+    # average variables and divide by the number
+    # of people at the end
+    avg_height += height
+    avg_weight += weight
+    avg_bmi += bmi
+
+    # One way of checking for a max or min is by
+    # comparing each with the currently set max/min.
+
+    if height > max_height:    # If this individual's height is greater than the current, that's the new max
+        max_height = height
+    if height < min_height:    # If this individual's height is less than the current, that's the new min
+        min_height = height
+
+    # Same goes for the weight and BMI
+    if weight > max_weight:
+        max_weight = weight
+    if weight < min_weight:
+        min_weight = weight
+
+    if bmi > max_bmi:
+        max_bmi = bmi
+    if bmi < min_bmi:
+        min_bmi = bmi
+    
+    n += 1    # Increment the amount of people
+
+    print("{}{:>12.2f}".format(line, bmi))    # Prints the line + the BMI column data
+
+# Prints all of the averages, maxes and mins in a separate section,
+# but in the same format. We divide the averages by the amount of people
+# we counted in 'n'. 
+print("\nAverage{:>9.2f}{:>13.2f}{:>12.2f}".format(avg_height / n, avg_weight / n, avg_bmi / n))
+print("Max{:>13}{:>13.2f}{:>12.2f}".format(max_height, max_weight, max_bmi))
+print("Min{:>13}{:>13.2f}{:>12.2f}".format(min_height, min_weight,min_bmi))
